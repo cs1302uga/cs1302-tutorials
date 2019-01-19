bf806951b4436f2bd49468b38ab4e2b9bbb535f1
Michael Cotterell <mepcotterell@gmail.com>
2019-01-18 19:09:21 -0500

    Update setup.sh

--- javadoc/setup.sh
+++ javadoc/setup.sh
@@ -4,8 +4,8 @@ if [ ! -d "cs1302-javadoc" ]; then
   git clone --depth 1 --no-checkout https://github.com/cs1302uga/cs1302-tutorials.git cs1302-javadoc
   cd cs1302-javadoc
   git checkout master -- javadoc
+  rm -f javadoc/setup.sh
   mv javadoc/* ./
   rm -rf javadoc
   rm -rf .git
-  rm -f setup.sh
 fi  
