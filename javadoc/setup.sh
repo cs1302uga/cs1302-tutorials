efcd485d88d97ab423f6f6c4af23b422846c10f0
Michael Cotterell <mepcotterell@gmail.com>
2019-01-18 19:16:07 -0500

    Update setup.sh

--- javadoc/setup.sh
+++ javadoc/setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 if [ ! -d "cs1302-javadoc" ]; then
-  git clone --depth 1 --no-checkout https://github.com/cs1302uga/cs1302-tutorials.git cs1302-javadoc
+  git clone --depth 1 --no-checkout https://github.com/cs1302uga/cs1302-tutorials.git cs1302-javadoc --quiet
   cd cs1302-javadoc
-  git checkout master -- javadoc
+  git checkout master -- javadoc --quiet
   rm -f javadoc/setup.sh
   mv javadoc/* ./
   rm -rf javadoc
   rm -rf .git
+  echo "subdirectory cs1302-javadoc successfully created"
+else
+  >&2 echo "subdirectory cs1302-javadoc already exists"
 fi  
