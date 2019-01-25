28e8891b31ee7a512c56014a1d4ed9eac90e2fdd
Brad Barnes <barnes@nike.cs.uga.edu>
2019-01-25 14:48:51 -0500

    updated setup.sh for this tutorial

--- default-methods/setup.sh
+++ default-methods/setup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-if [ ! -d "cs1302-interfaces" ]; then
-  git clone --depth 1 --no-checkout https://github.com/cs1302uga/cs1302-tutorials.git cs1302-interfaces
-  cd cs1302-interfaces
-  git checkout master -- interfaces
-  rm -f interfaces/setup.sh
-  mv interfaces/* ./
-  rm -rf interfaces
+TUTNAME="default-methods"
+DIR="cs1302-$TUTNAME"
+if [ ! -d DIR ]; then
+  git clone --depth 1 --no-checkout https://github.com/cs1302uga/cs1302-tutorials.git $DIR
+  cd $DIR
+  git checkout master -- $TUTNAME
+  rm -f $TUTNAME/setup.sh
+  mv $TUTNAME/* ./
+  rm -rf $TUTNAME
   rm -rf .git
-  mkdir bin doc
-  echo "subdirectory cs1302-interfaces successfully created"
+  mkdir -p bin doc
+  echo "subdirectory $DIR successfully created"
 else
-  >&2 echo "subdirectory cs1302-interfaces already exists"
+  >&2 echo "subdirectory $DIR already exists"
 fi  
