7f4e2c03aef1aec2e4a717361f1bf6efe5a7232c
bjb211 <bjb211@uga.edu>
2019-08-06 15:59:21 -0400

    added the interfaces directory to the tracked files list. It was incorrectly removed.

---
+++ interfaces/src/cs1302/interfaces/impl/Fancy.java
@@ -0,0 +1,47 @@
+package cs1302.interfaces.impl;
+
+import cs1302.interfaces.contract.Styleable;
+
+/**
+ * This class representes a <i>fancy</i> message.
+ * <p>
+ */
+public class Fancy implements Styleable {
+
+    private String message;
+    private boolean styled;
+
+    /**
+     * Constructs a new {@code Fancy} object with a specified
+     * message.
+     * {@code Fancy} objects are initially unstyled.
+     * @param msg the message to store in the object.
+     */
+    public Fancy(String msg) {
+        message = msg;
+        styled = false;
+    } // Secret
+
+    public void style() {
+        styled = true;
+    } // style
+
+    public void unstyle() {
+        styled = false;
+    } // unstyle
+
+    /**
+     * Returns a {@code String} representation of this {@code Fancy}
+     * object, consistent with {@link cs1302.interfaces.contract.Styleable}.
+     * @return the {@code String} value of this object
+     */
+    public String toString() {
+        String content;
+        if(styled) {
+            content = "*** " + message + " ***";
+        } else {
+            content = message;
+        } // if
+        return String.format("Fancy(%s)", content);
+    } // toString
+} // Fancy
---
+++ interfaces/src/cs1302/interfaces/impl/SuperFancy.java
@@ -0,0 +1,55 @@
+package cs1302.interfaces.impl;
+
+import cs1302.interfaces.contract.Styleable;
+
+/**
+ * This class representes a <i>super fancy</i> message.
+ * <p>
+ */
+public class SuperFancy implements Styleable {
+
+    private String message;
+    private boolean styled;
+
+    /**
+     * Constructs a new {@code SuperFancy} object with a specified
+     * message.
+     * {@code SuperFancy} objects are initially unstyled.
+     * @param msg the message to store in the object.
+     */
+    public SuperFancy(String msg) {
+        message = msg;
+        styled = false;
+    } // Secret
+
+    public void style() {
+        styled = true;
+    } // style
+
+    public void unstyle() {
+        styled = false;
+    } // unstyle
+
+    /**
+     * Returns a {@code String} representation of this {@code SuperFancy}
+     * object, consistent with {@link cs1302.interfaces.contract.Styleable}.
+     * @return the {@code String} value of this object
+     */
+    public String toString() {
+        String content = "";
+        if(styled) {
+            // Alternate upper and lower case for each letter
+            for(int i = 0; i < message.length(); i++) {
+                if(i % 2 == 0) {
+                    content += Character.toUpperCase(message.charAt(i));
+                } else {
+                    content += Character.toLowerCase(message.charAt(i));
+                } // if
+            } // for
+            content = "*** " + content + " ***";
+        } else {
+            content = message;
+        } // if
+        return String.format("Super Fancy(%s)", content);
+    } // toString
+} // Fancy
