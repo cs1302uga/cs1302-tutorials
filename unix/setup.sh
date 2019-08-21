ee64f4ca75dbe87181ceebacedcd83e1a34da86e
Michael Cotterell <mepcotterell@gmail.com>
2019-08-21 16:19:44 -0400

    Create setup.sh

---
+++ unix/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TUTNAME="unix"
+DIR="cs1302-$TUTNAME"
+if [ ! -d $DIR ]; then
+  mkdir $DIR
+  cd $DIR
+  mkdir -p notes/cs1302
+  mkdir -p notes/cs1302/ce00
+  mkdir -p notes/cs1302/ce01
+  mkdir -p notes/cs1730
+  echo "# ce00 NOTES" > notes/cs1302/ce00/NOTES.md
+  echo "# ce01 NOTES" > notes/cs1302/ce01/NOTES.md
+  echo "# README" > README.md
+else
+  >&2 echo "subdirectory $DIR already exists"
+fi  
