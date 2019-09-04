d59acc372d749635bd116902d5afa75e142b7308
Michael Cotterell <mepcotterell@gmail.com>
2019-09-04 12:50:47 -0400

    Update Driver.java

--- jdb/src/cs1302/jdb/Driver.java
+++ jdb/src/cs1302/jdb/Driver.java
@@ -5,6 +5,20 @@ package cs1302.jdb;
  */
 public class Driver {
 
+    /**
+     * Computes and returns the sum of the elements in {@code nums}.
+     *
+     * @param nums  numbers to sum
+     * @return sum of {@code nums}
+     */
+    public static double computeSum(double[] nums) {
+        double sum = 0.0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            sum += nums[i];
+        } // for
+        return sum;
+    } // computeSum
+    
     public static void main(String[] args) {
     
     
