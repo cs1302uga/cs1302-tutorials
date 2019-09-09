e6fd35e362ee4293d6a8a30c54170379261af412
Michael Cotterell <mepcotterell@gmail.com>
2019-09-09 17:56:07 -0400

    Create Driver.java

---
+++ varargs/src/cs1302/util/Driver.java
@@ -0,0 +1,15 @@
+package cs1302.util;
+
+/**
+ * Test driver app.
+ */
+public class Driver {
+
+    public static void main(String[] args) {
+        Helper.printlns(System.out, new String[] { "a", "b", "c" });
+        Helper.printlns(System.out, new String[] { "d", "e" });
+        Helper.printlns(System.out, new String[] { "f" });
+        Helper.printlns(System.out, new String[] { "g", "h", "i", "j", "k" });
+    } // main
+
+} // Driver
