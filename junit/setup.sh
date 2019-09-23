72b4d44adf9f697af5e48c1b88aaeb6b9ecd30de
Michael Cotterell <mepcotterell@gmail.com>
2019-09-23 17:46:07 -0400

    Update setup.sh

--- junit/setup.sh
+++ junit/setup.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
 TUTNAME="junit"
+JUNIT_VER="1.5.2"
+JUNIT_JAR="junit-platform-console-standalone-$JUNIT_VER.jar"
+JUNIT_URL="https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/$JUNIT_VER/$JUNIT_JAR"
 DIR="cs1302-$TUTNAME"
+
 if [ ! -d $DIR ]; then
   mkdir $DIR
   cd $DIR
   mkdir -p src bin doc test lib
   cd lib
-  curl -O https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/1.5.2/junit-platform-console-standalone-1.5.2.jar
+  echo "Downloading $JUNIT_JAR"
+  curl -O $JUNIT_URL
   cd ..
-  
+  echo "Downloading ConsoleLauncher"
+  curl -O https://raw.githubusercontent.com/cs1302uga/cs1302-tutorials/master/junit/ConsoleLauncher
+  chmod u+x ConsoleLauncher
   echo "subdirectory $DIR successfully created"
+  echo "- JUnit $JUNIT_VER dependencies are in $DIR/lib/$JUNIT_JAR"
+  echo "- while in $DIR, you can use './ConsoleLauncher' to run JUnit $JUNT_VER"
 else
   >&2 echo "subdirectory $DIR already exists"
 fi  
