78e34828ef32cb55e462cb16e722034c9c2d895d
Brad Barnes <bjb211@users.noreply.github.com>
2020-08-31 01:17:37 -0400

    Update SuperFancy.java

--- interfaces/src/cs1302/interfaces/impl/SuperFancy.java
+++ interfaces/src/cs1302/interfaces/impl/SuperFancy.java
@@ -22,17 +22,11 @@ public class SuperFancy implements Styleable {
         styled = false;
     } // SuperFancy
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public void style() {
         styled = true;
     } // style
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public void unstyle() {
         styled = false;
