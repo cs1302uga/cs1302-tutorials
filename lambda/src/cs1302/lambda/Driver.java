384044432cbb226385e87c763c64f3543240f250
Michael E. Cotterell <mepcotterell@gmail.com>
2021-10-18 16:30:10 -0400

    updated lambda starter code

--- lambda/src/cs1302/lambda/Driver.java
+++ lambda/src/cs1302/lambda/Driver.java
@@ -1,12 +1,25 @@
 package cs1302.lambda;
 
 import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 /** Simple driver for {@code cs1302-lambda} tutorial. */
 public class Driver {
 
     public static void main(String[] args) {
-        List<String> list = List.<String>of("hello", "world", "generic", "lambdas!");
+
+        List<Employee> employees = Lists.EMPLOYEES; // from Lists.java
+
+        // Consumer<Employee> println = void accept(Employee t) {
+        //     System.out.println(t);
+        // };
+        Consumer<Employee> println = null; // REPLACE null
+        Pipeline.<Employee>from(employees).forEach(println);
+
     } // main
 
 } // Driver
