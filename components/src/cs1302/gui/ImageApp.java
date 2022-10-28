f90ecf0847e4a567f8bd63e4a3fe5e3bab496465
Brad Barnes <bjb211@uga.edu>
2022-10-28 19:12:02 -0400

    Updated ImageApp to use methods from JavaFX lifecycle.

--- components/src/cs1302/gui/ImageApp.java
+++ components/src/cs1302/gui/ImageApp.java
@@ -14,22 +14,19 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.Priority;
 
 /**
- * A basic JavaFX 17 program which takes a user specified URL and loads it
+ * A basic JavaFX 8 program which takes a user specified URL and loads it
  * into an {@code ImageView}.
  *
  */
 public class ImageApp extends Application {
-
-    Stage stage;
-    Scene scene;
 
     /** The root container for the application scene graph */
     VBox vbox;
 
     /** The container for the url textfield and the load image button */
     HBox urlLayer;
-    TextField urlField;
-    Button loadImage;
+    TextField url;
+    Button loadButton;
 
     /** The container for the loaded image */
     ImageView imgView;
@@ -42,67 +39,90 @@ public class ImageApp extends Application {
     private static final int DEF_HEIGHT = 500;
     private static final int DEF_WIDTH = 500;
 
+    public ImageApp() {
+        System.out.println("2) Creating an instance of the ImageApp Application");
+
+        // Initialize the instance variables
+        vbox = new VBox();
+        urlLayer = new HBox(8);
+        imgView = new ImageView();
+        loadButton = new Button("Load");
+        url = new TextField("https://");
+    } // ImageApp
+
+        @Override
+    public void init() {
+        System.out.println("3) Executing the init method");
+
+        // Connect the components in the scene graph
+        vbox.getChildren().addAll(urlLayer, imgView);
+        urlLayer.getChildren().addAll(url, loadButton);
+        HBox.setHgrow(url, Priority.ALWAYS);
+        // load the default image
+        Image defaultImage = new Image(DEFAULT_IMG);
+
+        // add the image to the imageview
+        imgView.setImage(defaultImage);
+
+        // Create the handler for our button using an anonymous class.
+        // This approach isn't used but it is left in so students can
+        // compare with the lambda below.
+        /*
+        EventHandler<ActionEvent> mouseClickHandler = new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent e) {
+                    this.loadImage(e);
+                } // handle
+            };
+        */
+
+        // Create the handler for our button using a lambda expression.
+        // This is a more concise than using an anonymous class but it
+        // accomplishes the same goal.
+        EventHandler<ActionEvent> mouseClickHandler = (ActionEvent e) -> {
+            this.loadImage(e);
+        };
+
+        loadButton.setOnAction(mouseClickHandler);
+    } // init
+
     /**
      * The entry point for our image viewer application.
      *
      * @param stage A reference to the stage object (window) created by the system.
-     */ 
+     */
     public void start(Stage stage) {
-        this.stage = stage;
-        
-        // Initializing the nodes for the scene graph
-        vbox = new VBox();
-        urlLayer = new HBox(10);
-        urlField = new TextField("https://");
-        loadImage = new Button("Load");
+        System.out.println("4) Executing the start method");
 
-        // Adding the textfield and load image button the the containing hbox
-        urlLayer.getChildren().addAll(urlField, loadImage);
-
-        // Sets the textfield to grow, as necessary, to fill the hbox
-        HBox.setHgrow(urlField, Priority.ALWAYS);
-
-        // Load the default image with the default dimensions
-        Image img = new Image(DEFAULT_IMG, DEF_HEIGHT, DEF_WIDTH, false, false);
-
-        // Add the image to its container and preserve the aspect ratio if resized
-        imgView = new ImageView(img);
-        imgView.setPreserveRatio(true);
-
-        // EventHandler for our button                                                                    
-        EventHandler<ActionEvent> handler = (ActionEvent e) -> loadImage(e);
-        // Connect the handler to the button                                                              
-        loadImage.setOnAction(handler);
-
-        // Add the hbox and imageview to the containing vbox and set the vbox
-        // to be the root of the scene
-        vbox.getChildren().addAll(urlLayer, imgView);
-        scene = new Scene(vbox);
+        // Add the root of the scene graph to the stage.
+        Scene scene = new Scene(vbox);
 
         // Set up the stage and set it to be visible
-        // stage.setResizable(false);
         stage.setScene(scene);
         stage.setTitle("1302 Image Viewer!");
         stage.sizeToScene();
         stage.show();
-        
     } // start
 
     /**
      * Students will provide javadoc comments here.
      *
      * @param e source event
-     */ 
+     */
     private void loadImage(ActionEvent e) {
-
         try {
-            Image newImg = new Image(urlField.getText(), DEF_HEIGHT, DEF_WIDTH, false, false);
+            Image newImg = new Image(url.getText(), DEF_HEIGHT, DEF_WIDTH, false, false);
             imgView.setImage(newImg);
         } catch(IllegalArgumentException iae) {
             System.out.println("The supplied URL is invalid");
         } // try
-        
     } // loadImage
-    
-} // ImageApp
 
+    @Override
+    public void stop() {
+        // Won't be used by us. Typically used for application cleanup.
+
+        System.out.println("6) Executing the stop method");
+    } // stop
+
+} // ImageApp
