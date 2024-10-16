569af58d9e963bfc2b281359de30f8fa2136705a
Brad Barnes <bjb211@users.noreply.github.com>
2024-10-16 13:51:47 -0400

    Update ExampleDriver.java

--- javafx/src/cs1302/gui/ExampleDriver.java
+++ javafx/src/cs1302/gui/ExampleDriver.java
@@ -16,7 +16,7 @@ public class ExampleDriver {
             Application.launch(ExampleApp.class, args);
         } catch (Exception e) {
             System.err.println("The exception listed below occurred. If it pertains to DISPLAY,");
-            System.err.println("then please logout, then log back in passing -XY to ssh.");
+            System.err.println("then please logout, then log back in passing -XYC to ssh.");
             System.err.println(e.printStackTrace());
             System.exit(1);
         } // try
